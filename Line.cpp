ee07dff90b8129d8d3eb9fef168167258cbabefc
Jonas Pfab <jpfab@uw2-140-10.local>
2019-03-12 15:10:43 -0700

    Add preprocessing steps and process video data

--- Line.cpp
+++ Line.cpp
@@ -6,8 +6,14 @@ Line::Line(double theta, double rho){
 	this->rho = rho;
 }
 
-double Line::getY (double x) {
+double Line::getY(double x) {
 	double thetaRadian = (theta * PI) / 180.0;
 
-	return (rho  - x * cos(thetaRadian))/sin(thetaRadian);
+	return (rho  - x * cos(thetaRadian)) / sin(thetaRadian);
+}
+
+double Line::getX(double y) {
+	double thetaRadian = (theta * PI) / 180.0;
+
+	return (rho - y * sin(thetaRadian)) / cos(thetaRadian);
 }
--- Preprocessing.cpp
+++ Preprocessing.cpp
@@ -1,21 +1,50 @@
 #include "Preprocessing.h"
 
+#define KERNEL_SIZE 5
+
 Mat filterLanes(Mat img) {
-	// Mat hsvImage;
-	// cvtColor(img, hsvImage, COLOR_BGR2HSV);
+	Mat hsvImg;
+	Mat grayImg;
+	cvtColor(img, hsvImg, COLOR_BGR2HSV);
+	cvtColor(img, grayImg, COLOR_BGR2GRAY);
 
-	// Mat yellowHueRange;
-	// Mat whiteHueRange;
-	// inRange(hsvImage, Scalar(20, 100, 100), Scalar(30, 255, 255), yellowHueRange);
-	// inRange(hsvImage, Scalar(200, 200, 200), Scalar(255, 255, 255), whiteHueRange);
+	Mat yellowHueRange;
+	Mat whiteHueRange;
+	Mat mask;
+	inRange(hsvImg, Scalar(20, 100, 100), Scalar(30, 255, 255), yellowHueRange);
+	inRange(img, Scalar(120, 120, 120), Scalar(255, 255, 255), whiteHueRange);
+	bitwise_or(yellowHueRange, whiteHueRange, mask);
+	bitwise_and(grayImg, mask, grayImg);
 
-	return img;
+	return grayImg;
 }
 
 Mat applyGaussianBlur(Mat img) {
+	GaussianBlur(img, img, Size(KERNEL_SIZE, KERNEL_SIZE), 0);
 	return img;
 }
 
 Mat applyCannyEdgeDetection(Mat img) {
+	Canny(img, img, 50, 150);
+	return img;
+}
+
+Mat regionOfInterest(Mat img) {
+	Mat mask(img.rows, img.cols, CV_8UC1, Scalar(0));
+
+	vector<Point> vertices;
+	vertices.push_back(Point(img.cols / 9, img.rows));
+	vertices.push_back(Point(img.cols - (img.cols / 9), img.rows));
+	vertices.push_back(Point((img.cols / 2) + (img.cols / 8), (img.rows / 2) + (img.rows / 10)));
+	vertices.push_back(Point((img.cols / 2) - (img.cols / 8), (img.rows / 2) + (img.rows / 10)));
+
+	// Create Polygon from vertices
+	vector<Point> ROI_Poly;
+	approxPolyDP(vertices, ROI_Poly, 1.0, true);
+
+	// Fill polygon white
+	fillConvexPoly(mask, &ROI_Poly[0], ROI_Poly.size(), 255, 8, 0);
+	bitwise_and(img, mask, img);
+
 	return img;
 }
